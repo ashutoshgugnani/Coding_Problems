c7ea1d8042d7ad6ff0251ebe73921ccd9b5442c8
ashutoshgugnani <48962438+ashutoshgugnani@users.noreply.github.com>
2020-07-10 11:18:17 +0530

    master

--- Codechef/juneLong/crdgame.cpp
+++ Codechef/juneLong/crdgame.cpp
@@ -14,6 +14,7 @@ int main(){
         cin>>n;
         ll a_t=0;
         ll b_t=0;
+        ll s=0;
         for(ll i=0;i<n;i++){
             ll a,b;
             cin>>a>>b;
@@ -23,8 +24,8 @@ int main(){
             while(a>0 && b>0){
                 if(a<0){a=0;}
                 if(b<0){b=0;}
-                int digita=a%10;
-                int digitb=b%10;
+                ll digita=a%10;
+                ll digitb=b%10;
                 suma+=digita;
                 sumb+=digitb;
                 a=(a-digita)/10;
@@ -32,18 +33,17 @@ int main(){
             }
             if(suma>sumb)
                 a_t+=1;
-            if(sumb>suma)
+            else if(sumb>suma)
                 b_t+=1;
-            if(sumb=suma){
-                a_t+=1;
-                b_t+=1;
-            } 
+            else if(suma==sumb)
+                s+=1;
+            else{}
         }
         if(a_t>b_t)
             cout<<"0"<<" "<<a_t<<endl;
         if(b_t>a_t)
             cout<<"1"<<" "<<b_t<<endl;
         if(a_t==b_t)
-            cout<<"2"<<" "<<a_t<<endl;
+            cout<<"2"<<" "<<a_t+s<<endl;
     }
 }
