6e92d50eb8c29969807535aece57ddf4ac91ea2b
Ashutosh Gugnani <ashutosh.gugnani@gmail.com>
2020-07-14 00:02:55 +0530

    master

--- Codechef/juneLong/adaking.cpp
+++ Codechef/juneLong/adaking.cpp
@@ -1,33 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
+int main() {
     int t;
-    cin>>t; 
-    while(t--){
-        char chessBoard[8][8];
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                chessBoard[i][j]='.';
-            }   
-        }
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                cout<<chessBoard[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-        int n;
-        cin>>n;
-        chessBoard[3][3]='O';
-
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                cout<<chessBoard[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
+    cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        char chess[8][8];
+        chess[0][0]='O';
+        k--;
+        for(int row=0;row<8;row++){
+            for(int column=0;column<8;column++)
+        {
+                if(row==0 && column==0)
+                continue;
+                if(k)
+                    {
+                        chess[row][column]='.';
+                        k--;
+                    }
+                else{
+                chess[row][column]='X';
+                    }
             
+        }}
+        for(int row=0;row<8;row++)
+        {
+            for(int column=0;column<8;column++)
+            {
+                cout<<chess[row][column];
+            }
+            cout<<endl;
+        }
+
     }
-    return 0;
 }
